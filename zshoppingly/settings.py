0e9d9f9e33e6d1c0b7d5db52298388dcda0fe958
PrathameshPC77 <prathameshpc3322@gmail.com>
2024-09-05 23:13:05 +0530

    changed path

--- zshoppingly/settings.py
+++ zshoppingly/settings.py
@@ -107,7 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
